055b6662f82ef33eb9fdffb960e6b7f0786e3f4c
bheem ratan <bheem501425@gmail.com>
2023-06-19 21:43:15 +0530

    port

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/',(req,res)=>{
   res.json({message:'server is woring fine jani',status:201})
 })
 
-const server = app.listen(8000, () => {
+const server = app.listen( process.env.PORT || 8000, () => {
   console.log('Server is running on port 8000');
 });
 
