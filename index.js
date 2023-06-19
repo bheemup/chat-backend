6d7a380bec69cfb26c9ea2c133eb31a15385dbf9
bheem ratan <bheem501425@gmail.com>
2023-06-19 12:06:26 +0530

    origin allow

--- index.js
+++ index.js
@@ -21,6 +21,12 @@ const io = socketIO(server, {
     origin: '*',
   },
 });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 io.on('connection', (socket) => {
 
